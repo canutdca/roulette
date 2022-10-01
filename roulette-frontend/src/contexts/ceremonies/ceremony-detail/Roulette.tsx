c5c5a4dc6e25bd2d961ffb89c645a405a0e06b44
David Canut <canutdca@gmail.com>
2022-10-01 10:33:08 +0200

    init canvas of roulette

--- roulette-frontend/src/contexts/ceremonies/ceremony-detail/Roulette.tsx
+++ roulette-frontend/src/contexts/ceremonies/ceremony-detail/Roulette.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled"
+import { useEffect, useRef } from "react"
 
 export interface RouletteProps {
 	members: string[]
@@ -6,37 +7,38 @@ export interface RouletteProps {
 
 export function Roulette({ members }: RouletteProps) {
 
-	return (
-		<span>hola</span>
-	)
+	const canvasRef = useRef<HTMLCanvasElement | null>(null)
+	const center = 200
+	useEffect(() => {
+		const canvas: HTMLCanvasElement = canvasRef.current!
+		const ctx = canvas!.getContext('2d')!
+		const center = 200
+
+
+		const complete = 2 * Math.PI
+		ctx.moveTo(center,center);
+		ctx.arc(center,center,center,0,complete/4);
+		ctx.lineTo(center,center);
+		ctx.fillStyle = 'blue'
+		ctx.font = "30px Arial";
+		ctx.fill()
+		
+		
+		ctx.save();
+		ctx.fillStyle = 'white'
+		ctx.translate(200, 200);
+		ctx.rotate(Math.PI/4);
+		ctx.fillText("David asd", 20, 10);
+		ctx.restore();
+
+
+	}, [canvasRef])
+
+	
+	return <Canvas ref={canvasRef} width="400" height="400"/>
 
 }
-const Container = styled.div`
-	position: fixed;
-	top: 0;
-	bottom: 0;
-	left: 0;
-	right: 0;
-	background-color: #33333333;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-`
-const Modal = styled.div`
-	background-color: #fff;
-	padding: 3em 2em;
-	border-radius: 3px;
-`
-
-const CandidateName = styled.div`
-	margin-top: 1rem;
-	margin-bottom: 2rem;
-	font-size: 1.2rem;
-	font-weight: bold;
-	text-align: center;
-`
-
-const ButtonContainer = styled.div`
-	display: flex;
-	justify-content: space-between
+const Canvas = styled.canvas`
+	border: 1px solid grey;
+	border-radius: 50%;
 `
